c840aecf18fe1a5440859b119474b0ad67890373
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-08-26 18:43:50 +0900

    [fix]typescript error about arg of onChange

--- src/component/index.d.ts
+++ src/component/index.d.ts
@@ -3,7 +3,7 @@ import { Component } from 'react'
 interface Props {
   className?: string
   fileContainerStyle?: object
-  onChange?: (files: File[]) => void
+  onChange?: (files: File[],pictures:string[]) => void
   buttonClassName?: string
   buttonStyles?: object
   withPreview?: boolean
